7a61d25d3325f342ff4024bfc337f58c88bac16a
Dhvani Vora <dvora@splunk.com>
2023-07-26 11:09:38 +0530

    Removed unused variable from consts

--- pastebin_consts.py
+++ pastebin_consts.py
@@ -14,11 +14,7 @@
 # and limitations under the License.
 #
 #
-# State file
-PASTEBIN_STATE_FILE_CORRUPT_ERR = (
-    "Error occurred while loading the state file due to its unexpected format."
-    "Resetting the state file with the default format. Please try again."
-)
+
 # Test Connectivity endpoints
 PASTEBIN_CONNECTION_MESSAGE = "Querying endpoint to verify the credentials provided"
 PASTEBIN_CONNECTIVITY_FAIL_MESSAGE = "Test connectivity failed"
